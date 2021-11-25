f4cce9af30cb8cf2c2f7963a0644090c9a4d3d95
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-11-25 17:10:22 +0100

    LeaningJS

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function sayHi(name) {
-    return `Hi there, ${name}`;
-  }
-  
-  console.log(sayHi());
+var status = '😎';
+
+setTimeout(() => {
+  const status = '😍';
+
+  const data = {
+    status: '🥑',
+    getStatus() {
+      return this.status;
+    },
+  };
+
+  console.log(data.getStatus());
+  console.log(data.getStatus.call(this));
+}, 0);
